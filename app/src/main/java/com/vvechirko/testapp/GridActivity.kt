a1ce7f444ff353f9fa85feb4e8acf003ede7fe4a
vvechirko <volodymyr.vechirko@globaltechmakers.com>
2018-09-28 10:37:15 +0300

    * grid recyclerView anim

--- app/src/main/java/com/vvechirko/testapp/GridActivity.kt
+++ app/src/main/java/com/vvechirko/testapp/GridActivity.kt
@@ -3,51 +3,71 @@ package com.vvechirko.testapp
 import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
-import android.view.*
+import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.view.animation.AnimationUtils
 import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class GridActivity : AppCompatActivity() {
 
     lateinit var rv: RecyclerView
-    lateinit var grid: GridLayoutManager
-    lateinit var list: LinearLayoutManager
+    lateinit var adapter: Adapter
+
+    val SPANS = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val content: FrameLayout = findViewById(android.R.id.content)
 
-        val adapter = Adapter()
-        list = LinearLayoutManager(this)
-        grid = GridLayoutManager(this, 2).apply {
-            spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
-                override fun getSpanSize(position: Int): Int {
-                    return if (adapter.getItemViewType(position) == adapter.div) 2 else 1
-                }
-            }
-        }
+        adapter = Adapter()
+        val grid = GridLayoutManager(this, 1)
 
+        val animation = AnimationUtils.loadLayoutAnimation(this, R.anim.layout_animation_fall_down)
         rv = RecyclerView(this)
+        rv.layoutAnimation = animation
         rv.layoutManager = grid
         rv.adapter = adapter
+        spanCount(SPANS)
 
         content.addView(rv, ViewGroup.LayoutParams(MATCH_PARENT, MATCH_PARENT))
     }
 
-    fun toggle() {
-        val pos = (rv.layoutManager as LinearLayoutManager).findFirstCompletelyVisibleItemPosition()
-        if (rv.layoutManager === grid) {
-            rv.layoutManager = list
-        } else {
-            rv.layoutManager = grid
+    override fun onStart() {
+        super.onStart()
+        adapter.setSections(
+                mutableListOf<String>().apply {
+                    for (i in 1..5) add("item $i")
+                },
+                mutableListOf<String>().apply {
+                    for (i in 1..8) add("item $i")
+                },
+                mutableListOf<String>().apply {
+                    for (i in 1..7) add("item $i")
+                }
+        )
+    }
+
+    fun spanCount(span: Int) {
+        with(rv.layoutManager as GridLayoutManager) {
+            spanCount = span
+            spanSizeLookup = adapter.spanSizeLookup(span)
         }
-        (rv.layoutManager as LinearLayoutManager).scrollToPosition(pos)
+    }
+
+    fun toggle() {
+        with(rv.layoutManager as GridLayoutManager) {
+            val pos = findFirstCompletelyVisibleItemPosition()
+            spanCount(if (spanCount == 1) SPANS else 1)
+            scrollToPosition(pos)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -64,36 +84,28 @@ class GridActivity : AppCompatActivity() {
 
 class Adapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    val div = -1
+    val DIVIDER = -1
 
-    var sections: MutableList<List<String>> = mutableListOf(
-            mutableListOf<String>().apply {
-                for (i in 1..13) add("item $i")
-            },
-            mutableListOf<String>().apply {
-                for (i in 1..15) add("item $i")
-            },
-            mutableListOf<String>().apply {
-                for (i in 1..11) add("item $i")
-            }
-    )
+    private var sections: MutableList<List<String>> = mutableListOf()
 
     override fun getItemViewType(position: Int): Int {
         var start: Int = 0
         sections.forEachIndexed { index, list ->
-            if (position == start) return div
+            if (position == start) return DIVIDER
             start += list.size + 1
             if (position < start) return index
         }
-        return div
+        return DIVIDER
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        // viewType is index of section or divider -1
         val view = when (viewType) {
-            div -> divider(parent.context)
             0 -> view1(parent.context)
             1 -> view2(parent.context)
-            else -> view3(parent.context)
+            2 -> view3(parent.context)
+//            DIVIDER -> divider(parent.context)
+            else -> divider(parent.context)
         }
         return object : RecyclerView.ViewHolder(view) {}
     }
@@ -101,11 +113,11 @@ class Adapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun getItemCount(): Int = sections.size + sections.sumBy { it.size }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        // viewType is index of section
+        // viewType is index of section or divider -1
         val viewType = getItemViewType(position)
         val correctPos = correctPosition(position)
 
-        if (viewType == div) {
+        if (viewType == DIVIDER) {
             (holder.itemView as TextView).setText("Section ${correctPos + 1}")
         } else {
             sections.get(viewType).get(correctPos).let {
@@ -122,6 +134,20 @@ class Adapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
             if (position < start) return position + list.size - start
         }
         return position
+    }
+
+    fun spanSizeLookup(span: Int) =
+            if (span == 1) GridLayoutManager.DefaultSpanSizeLookup()
+            else object : GridLayoutManager.SpanSizeLookup() {
+                override fun getSpanSize(position: Int): Int {
+                    return if (getItemViewType(position) == DIVIDER) span else 1
+                }
+            }
+
+    fun setSections(vararg items: List<String>) {
+        sections.clear()
+        sections.addAll(items)
+        notifyDataSetChanged()
     }
 
 //  =========================================
